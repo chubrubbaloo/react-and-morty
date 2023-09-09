15add45d96edeffb701f0a04e191194c1eeb8dc6
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-09 16:38:13 +0200

    📝docs: added further documentation for the characterlist

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 import { fetchData } from '../api/apiHandler';
 
 function CharacterList() {
+  // State variables to store characters, current and total pages
   const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-
+  // Use useEffect to fetch characters when the component mounts or when the current page changes
   useEffect(() => {
     async function fetchCharacters(page) {
       try {
+        // Fetch character data for the specified page
         const data = await fetchData(`character/?page=${page}`);
         setCharacters(data.results);
         setTotalPages(data.info.pages);
@@ -19,15 +21,18 @@ function CharacterList() {
       }
     }
 
+    // Call fetchCharacters with the current page 
     fetchCharacters(currentPage);
   }, [currentPage]);
 
+  // Function to handle moving to the previous page
   function handlePreviousPage() {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   }
 
+  // Function to handle moving to the next page
   function handleNextPage() {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
@@ -40,12 +45,14 @@ function CharacterList() {
       <ul>
         {characters.map((character) => (
           <li key={character.id}>
+            {/* Create links to individual character detail pages */}
             <Link to={`/character/${character.id}`}>{character.name}</Link>
           </li>
         ))}
       </ul>
 
       <div>
+        {/* Disables prev and next buttons if on first or last page */}
         <button onClick={handlePreviousPage} disabled={currentPage === 1}>
           Previous
         </button>
