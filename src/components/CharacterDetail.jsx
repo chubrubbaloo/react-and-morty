37a6e17ca62b075e5a456dfbbb2c30d41e56f3bd
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-09 16:42:48 +0200

    📝docs: added further documentation for the characterDetail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,18 +1,18 @@
-// CharacterDetail.js
-
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchData } from '../api/apiHandler';
 
 function CharacterDetail() {
-  const { id } = useParams(); // Get the character ID from the URL params
+  const { id } = useParams(); // Get the character ID from the URL params using the useParams hook
   const [character, setCharacter] = useState(null);
 
+  // Use the useEffect hook to fetch character data when the component mounts
   useEffect(() => {
     async function fetchCharacter() {
       try {
-        // Fetch the specific character by ID from the API
+        // Fetch the specific character by ID from the API using fetchData
         const data = await fetchData(`character/${id}`);
+        // Update the character state with the fetched data
         setCharacter(data);
       } catch (error) {
         // Handle any errors that occur during the API request
@@ -23,10 +23,12 @@ function CharacterDetail() {
     fetchCharacter();
   }, [id]);
 
+  // If character data has not been fetched yet, display a loading message
   if (!character) {
     return <div>Loading...</div>;
   }
 
+  // Once character data is available, render the character details
   return (
     <div>
       <h1>Character Details</h1>
