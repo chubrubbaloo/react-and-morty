382b2d44e28f2f0b5d9ba8faddd68735e35a17f3
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-10 11:47:49 +0200

    📝docs: added further documentation for the apihandler

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -1,13 +1,14 @@
-// apiHandler.js
-
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
+// Generic fetch function where you set the endpoint after the base url
 async function fetchData(endpoint) {
   try {
+    // If the response back from the server is not ok throw new error
     const response = await fetch(`${BASE_URL}/${endpoint}`);
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
     }
+    // returns the parsed data back
     const data = await response.json();
     return data;
   } catch (error) {
