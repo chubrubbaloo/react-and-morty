b8685473d04f8e2cb398bad5db2f98c226ea5526
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-10 11:25:39 +0200

    ✨feat: added search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function SearchBar({ onSearch }) {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const handleSearch = () => {
+        onSearch(searchTerm);
+    }
+
+    return (
+        <div>
+            <input
+                type='text'
+                placeholder='Search for a character...'
+                value={searchTerm}
+                onChange={handleInputChange}
+            />
+        </div>
+    )
+}
