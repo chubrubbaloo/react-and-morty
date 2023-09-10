e81a8671664af9b36f722c787da33072a3ec4236
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-10 12:34:21 +0200

    💄style: implemented bootstrap styling for the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function SearchBar({ onSearch }) {
-
     const [searchTerm, setSearchTerm] = useState('');
 
     const handleInputChange = (e) => {
-        // set the search term to the input field
         const newSearchTerm = e.target.value;
         setSearchTerm(newSearchTerm);
-
-        // call the search function as the user types in the input field
         onSearch(newSearchTerm);
     }
 
     return (
-        <div>
-            <input
-                type='text'
-                placeholder='Search for a character...'
-                value={searchTerm}
-                onChange={handleInputChange}
-            />
+        <div className="d-flex justify-content-center align-items-center mb-3">
+            <div className="input-group" style={{width: '80%'}}>
+                <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Search for a character"
+                    placeholder="Search for a character..."
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+            </div>
         </div>
     )
 }
