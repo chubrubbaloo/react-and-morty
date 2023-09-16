345de8d68fba04677a8235bf4766eb4e9874e3f7
Haris Avdicevic <haris.avdi93@gmail.com>
2023-09-16 14:34:57 +0200

    ✨feat: able to display all episodes of character

--- src/pages/CharacterDetailPage.jsx
+++ src/pages/CharacterDetailPage.jsx
@@ -5,7 +5,7 @@ import { fetchData } from '../api/apiHandler';
 function CharacterDetailPage() {
   const { id } = useParams(); // Get the character ID from the URL params using the useParams hook
   const [character, setCharacter] = useState(null);
-  const [firstEpisode, setFirstEpisode] = useState('');
+  const [episodes, setEpisodes] = useState([]);
 
   // Use the useEffect hook to fetch character data when the component mounts
   useEffect(() => {
@@ -15,29 +15,44 @@ function CharacterDetailPage() {
         const data = await fetchData(`character/${id}`);
         // Update the character state with the fetched data
         setCharacter(data);
+        const episodeURLs = data.episode;
+
+        // Use Promise.all to fetch data for all episodes simultaneously
+        const episodePromises = episodeURLs.map(async (url) => {
+          const episodeResponse = await fetch(url);
+          if (episodeResponse.status === 200) {
+            return episodeResponse.json();
+          } else {
+            throw new Error(`Error fetching episode data. Status: ${episodeResponse.status}`);
+          }
+        });
+
+        // Wait for all episode data to be fetched
+        const episodeDataArray = await Promise.all(episodePromises);
+
+        // Extract episode details (name, episode code, air date)
+        const episodeDetails = episodeDataArray.map((episodeData) => {
+          return {
+            name: episodeData.name,
+            episode: episodeData.episode,
+            air_date: episodeData.air_date,
+          };
+        });
+
+        // Update the state with episode details
+        setEpisodes(episodeDetails);
       } catch (error) {
         // Handle any errors that occur during the API request
         console.error('Error fetching character:', error);
       }
     }
 
-    async function fetchEpisode() {
-      const data2 = await fetchData(`episode/1`);
-      console.log(data2);
-      setFirstEpisode(data2);
-    }
-
-    fetchEpisode()
-
-
-
-
     fetchCharacter();
   }, [id]);
 
   // If character data has not been fetched yet, display a loading message
   if (!character) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   // Function to get the appropriate status indicator color
@@ -50,19 +65,38 @@ function CharacterDetailPage() {
       default:
         return 'bg-secondary'; // Grey
     }
-  }
-
+  };
 
   // Once character data is available, render the character details
   return (
     <div>
       <img src={character.image} alt={character.name} />
       <h2>{character.name}</h2>
-      <p className="card-text"> {character.status} - {character.species} <span className={`rounded-circle me-2 ${getStatusIndicatorColor(character.status)}`} style={{ width: '10px', height: '10px', display: 'inline-block' }}></span> </p>
+      <p className="card-text">
+        Status: {character.status}{'  '}
+        <span
+          className={`rounded-circle me-2 ${getStatusIndicatorColor(character.status)}`}
+          style={{ width: '10px', height: '10px', display: 'inline-block' }}
+        ></span>{' '}
+      </p>
+      <p>Species: {character.species} </p>
       <p>Gender: {character.gender} </p>
       <p>Origin: {character.origin.name} </p>
       <p>Last known location: {character.location.name} </p>
-      <p>First seen:  {firstEpisode.episode} - {firstEpisode.name} </p>
+      <p>Has been seen in: </p>
+      <ul>
+        {episodes.map((episode, index) => (
+          <li key={index}>
+
+            Episode - {episode.episode}
+            <br />
+            Name - {episode.name}
+            <br />
+            Air date - {episode.air_date}
+            <hr />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
