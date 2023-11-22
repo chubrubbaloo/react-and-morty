a991ca476b5c64b23a0138fe1aa5ab953f1cae40
Haris Avdicevic <haris.avdicevic@fortnox.se>
2023-11-22 19:29:52 +0100

    feat: implemented table for all episode data

--- src/pages/CharacterDetailPage.jsx
+++ src/pages/CharacterDetailPage.jsx
@@ -1,107 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { fetchData } from '../api/apiHandler';
-import {CircularProgress} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {fetchData} from '../api/apiHandler';
+import {CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import '../pages/CharacterDetailPage.css'
 
 function CharacterDetailPage() {
-  const { id } = useParams(); // Get the character ID from the URL params using the useParams hook
-  const [character, setCharacter] = useState(null);
-  const [episodes, setEpisodes] = useState([]);
+    const {id} = useParams();
+    const [character, setCharacter] = useState(null);
+    const [episodes, setEpisodes] = useState([]);
 
-  // Use the useEffect hook to fetch character data when the component mounts
-  useEffect(() => {
-    async function fetchCharacter() {
-      try {
-        // Fetch the specific character by ID from the API using fetchData
-        const data = await fetchData(`character/${id}`);
-        // Update the character state with the fetched data
-        setCharacter(data);
-        const episodeURLs = data.episode;
+    useEffect(() => {
+        async function fetchCharacter() {
+            try {
+                const data = await fetchData(`character/${id}`);
+                setCharacter(data);
+                const episodeURLs = data.episode;
 
-        // Use Promise.all to fetch data for all episodes simultaneously
-        const episodePromises = episodeURLs.map(async (url) => {
-          const episodeResponse = await fetch(url);
-          if (episodeResponse.status === 200) {
-            return episodeResponse.json();
-          } else {
-            throw new Error(`Error fetching episode data. Status: ${episodeResponse.status}`);
-          }
-        });
+                const episodePromises = episodeURLs.map(async (url) => {
+                    const episodeResponse = await fetch(url);
+                    if (episodeResponse.status === 200) {
+                        return episodeResponse.json();
+                    } else {
+                        throw new Error(`Error fetching episode data. Status: ${episodeResponse.status}`);
+                    }
+                });
 
-        // Wait for all episode data to be fetched
-        const episodeDataArray = await Promise.all(episodePromises);
+                const episodeDataArray = await Promise.all(episodePromises);
 
-        // Extract episode details (name, episode code, air date)
-        const episodeDetails = episodeDataArray.map((episodeData) => {
-          return {
-            name: episodeData.name,
-            episode: episodeData.episode,
-            air_date: episodeData.air_date,
-          };
-        });
+                const episodeDetails = episodeDataArray.map((episodeData) => {
+                    return {
+                        name: episodeData.name,
+                        episode: episodeData.episode,
+                        air_date: episodeData.air_date,
+                    };
+                });
+                setEpisodes(episodeDetails);
+            } catch (error) {
+                console.error('Error fetching character:', error);
+            }
+        }
 
-        // Update the state with episode details
-        setEpisodes(episodeDetails);
-      } catch (error) {
-        // Handle any errors that occur during the API request
-        console.error('Error fetching character:', error);
-      }
+        fetchCharacter();
+    }, [id]);
+
+    if (!character) {
+        return <div className="m-5">
+            <CircularProgress size="10em" color="success"/>
+            <h2 className="m-5">Loading...</h2>
+        </div>;
     }
 
-    fetchCharacter();
-  }, [id]);
-
-  // If character data has not been fetched yet, display a loading message
-  if (!character) {
-    return <div style={{margin: '2em'}}>
-      <CircularProgress  color="success"/>
-      <h2>Loading...</h2>
-    </div>;
-  }
-
-  // Function to get the appropriate status indicator color
-  const getStatusIndicatorColor = (status) => {
-    switch (status) {
-      case 'Alive':
-        return 'bg-success'; // Green
-      case 'Dead':
-        return 'bg-danger'; // Red
-      default:
-        return 'bg-secondary'; // Grey
-    }
-  };
-
-  // Once character data is available, render the character details
-  return (
-    <div>
-      <img src={character.image} alt={character.name} />
-      <h2>{character.name}</h2>
-      <p className="card-text">
-      {character.status} - {character.species} {'  '}
-        <span
-          className={`rounded-circle me-2 ${getStatusIndicatorColor(character.status)}`}
-          style={{ width: '10px', height: '10px', display: 'inline-block' }}
-        ></span>
-      </p>
-      <p>Gender: {character.gender} </p>
-      <p>Origin: {character.origin.name} </p>
-      <p>Last known location: {character.location.name} </p>
-      <p>Has been seen in: </p>
-      <ul>
-        {episodes.map((episode, index) => (
-          <li key={index}>
-
-            Episode - {episode.episode}
-            <br />
-            Name - {episode.name}
-            <br />
-            Air date - {episode.air_date}
-            <hr />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <img className="mt-4" src={character.image} alt={character.name}/>
+            <h2 className="mt-4">{character.name}</h2>
+            <TableContainer className="table-container" component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align='center' className="column-title">Episode</TableCell>
+                            <TableCell align='center' className="column-title">Episode Name</TableCell>
+                            <TableCell align='center' className="column-title">Air Date</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {episodes.map((episode, index) => (
+                            <TableRow key={index}>
+                                <TableCell className="text-center">{episode.episode}</TableCell>
+                                <TableCell className="text-center">{episode.name}</TableCell>
+                                <TableCell className="text-center">{episode.air_date}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
 }
 
 export default CharacterDetailPage;
