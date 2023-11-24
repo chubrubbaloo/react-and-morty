8c37fe5c4fe5585f9096c79daf60cf3ea215308c
Haris Avdicevic <haris.avdicevic@fortnox.se>
2023-11-24 17:08:29 +0100

    refactor: removed unused code when fetching all characters

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -8,40 +8,21 @@ import '../pages/CharactersPageStyle.css'
 function CharactersPage() {
     const initialPage = parseInt(localStorage.getItem('currentPage') || 1);
 
-    // State variables to store characters, current and total pages
     const [characters, setCharacters] = useState([]);
     const [currentPage, setCurrentPage] = useState(initialPage);
     const [totalPages, setTotalPages] = useState(1);
 
-    // Use useEffect to fetch characters when the component mounts or when the current page changes
     useEffect(() => {
         async function fetchCharacters(page) {
             try {
-                // Fetch character data for the specified page
                 const data = await fetchData(`character/?page=${page}`);
                 setCharacters(data.results);
                 setTotalPages(data.info.pages);
-
-                // Fetch and set the first episode for each character
-                const charactersWithFirstEpisodes = await Promise.all(
-                    data.results.map(async (character) => {
-                        const firstEpisodeUrl = character.episode[0];
-                        const firstEpisodeResponse = await fetch(firstEpisodeUrl);
-                        if (firstEpisodeResponse.status === 200) {
-                            const firstEpisodeData = await firstEpisodeResponse.json();
-                            character.firstEpisode = firstEpisodeData.name;
-                        }
-                        return character;
-                    })
-                );
-
-                setCharacters(charactersWithFirstEpisodes);
             } catch (error) {
                 console.error('Error fetching characters:', error);
             }
         }
 
-        // Call fetchCharacters with the current page
         fetchCharacters(currentPage);
     }, [currentPage]);
 
@@ -49,19 +30,17 @@ function CharactersPage() {
         localStorage.setItem('currentPage', currentPage.toString());
     }, [currentPage]);
 
-    // Function to handle moving to the previous page
     async function handlePreviousPage() {
         if (currentPage > 1) {
             await setCurrentPage(currentPage - 1);
-            //window.scrollTo(0, 0);
+            window.scrollTo(0, 0);
         }
     }
 
-    // Function to handle moving to the next page
     async function handleNextPage() {
         if (currentPage < totalPages) {
             await setCurrentPage(currentPage + 1);
-            // window.scrollTo(0, 0)
+            window.scrollTo(0, 0)
 
         }
     }
@@ -98,24 +77,18 @@ function CharactersPage() {
         <>
             <h1 className="mt-3">Rickipedia</h1>
             <SearchBar onSearch={searchCharacters}/>
-
             <Grid
                 container
                 justifyContent="center"
             >
-
                 {characters.map((character) => (
                     <Grid className="grid-item m-3" key={character.id}>
-
                         <Link className="no-text-decoration" to={`/character/${character.id}`}>
-
                             <Paper elevation={2}>
-
                                 <img src={character.image} alt={character.name}/>
                                 <h4 className="card-title mt-2 mb-4">
                                     {character.name}
                                 </h4>
-
                                 Current Status: <b> {character.status} </b> {' '}
                                 <span
                                     className="status"
@@ -123,13 +96,10 @@ function CharactersPage() {
                                         border: `5px solid ${getStatusIndicatorColor(character.status)}`
                                     }}>
                                 </span>
-
                                 <p className="mt-3">Species: <b>{character.species}</b></p>
-
                                 Origin:
                                 <br/>
                                 <div className="pb-4"><b>{character.origin.name}</b></div>
-
                                 Last known location:
                                 <br/>
                                 <div className="pb-4"><b>{character.location.name}</b></div>
@@ -141,7 +111,6 @@ function CharactersPage() {
 
 
             <div>
-                {/* Disables prev and next buttons if on the first or last page */}
                 <Button
                     variant="contained"
                     onClick={handlePreviousPage}
