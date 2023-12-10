413282e698ba82df0a1b326a00a63cfb2e6b87d1
Haris Avdicevic <haris.avdicevic@fortnox.se>
2023-12-10 08:45:41 +0100

    test: implemented tests for each case in characterDetailsPage

--- cypress/e2e/characterDetailsPageTests/characterDetailsPageTests.cy.ts
+++ cypress/e2e/characterDetailsPageTests/characterDetailsPageTests.cy.ts
@@ -1,15 +1,19 @@
 describe('Character details page ', () => {
 
-    // Write test for routing to alber
     it('When clicking on Albert Einstein it should route to his specific detail page', () => {
         cy.visit('http://localhost:3000');
 
-        cy.get('.grid-item')
-            .eq(10)
-            .find('a')
-            .click();
+        cy.get('.grid-item').eq(10).find('a').click();
 
         cy.url().should('include', '/character/11');
+    })
+
+    it('The image of the character should be of Albert Einstein and the alt should be Albert Einstein', () => {
+        cy.visit('http://localhost:3000/character/11');
+
+        cy.get('.center-image img').should('have.attr', 'src', 'https://rickandmortyapi.com/api/character/avatar/11.jpeg');
+        cy.get('.center-image img').should('have.attr', 'alt', 'Albert Einstein');
+
     })
 
     it('The characters name should be Albert Einstein', () => {
@@ -18,6 +22,40 @@ describe('Character details page ', () => {
         const characterName = 'Albert Einstein';
 
         cy.get('h2').contains(characterName);
+    })
+
+    it('The table head names should contain Episode, Episode Name and Air Date', () => {
+        cy.visit('http://localhost:3000/character/11');
+
+        cy.get('.MuiTableCell-root')
+            .eq(0)
+            .contains('Episode');
+
+        cy.get('.MuiTableCell-root')
+            .eq(1)
+            .contains('Episode Name');
+
+        cy.get('.MuiTableCell-root')
+            .eq(2)
+            .contains('Air Date');
+    });
+
+    it('The table cells should contain the correct data', () => {
+        cy.visit('http://localhost:3000/character/11');
+
+        cy.get('.MuiTableCell-root')
+            .eq(3)
+            .contains('S02E01');
+
+        cy.get('.MuiTableCell-root')
+            .eq(4)
+            .contains('A Rickle in Time');
+
+        cy.get('.MuiTableCell-root')
+            .eq(5)
+            .contains('July 26, 2015');
+
+
     })
 })
 
